ac80b596dfb35dead1c551f48bf9dd00a9ebc4b1
aurelmichi6-gif <aurelmichi6@gmail.com>
2026-04-09 17:04:02 +0700

    Update gemini.js

--- api/gemini.js
+++ api/gemini.js
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
                     model: "llama-3.1-8b-instant",
                     messages: [{
                         role: "user",
-                        content: "*You are playing a drum judge game based on AI gemini that scored. Answer this situation/question with a short, safe, on-topic, calm, logic, appropriate for all ages, and likely not an AI response (max 75 characters)*: " + question
+                        content: "*You are playing a drum judge game based on AI that scored. Answer this situation/question with a short, safe, on-topic, calm, logic, appropriate for all ages, and likely not an AI response (max 75 characters)*: " + question
                     }],
                     max_tokens: 100
                 })
