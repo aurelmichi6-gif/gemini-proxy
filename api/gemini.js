030de0297b7368a034d258428955ce420284b156
aurelmichi6-gif <aurelmichi6@gmail.com>
2026-04-09 16:45:31 +0700

    Update gemini.js

--- api/gemini.js
+++ api/gemini.js
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
                     model: "llama-3.1-8b-instant",
                     messages: [{
                         role: "user",
-                        content: "*You are playing a funny party game. Answer this situation/question with a short, safe, on-topic, calm, smart, logic, appropriate but funny response (max 75 characters)*: " + question
+                        content: "*You are playing a funny party game. Answer this situation/question with a short, safe, on-topic, calm, logic, appropriate response (max 75 characters)*: " + question
                     }],
                     max_tokens: 100
                 })
