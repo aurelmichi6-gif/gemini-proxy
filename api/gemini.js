3683d5fea29a26662bb7efe59dca3dd9170619c3
aurelmichi6-gif <aurelmichi6@gmail.com>
2026-04-13 07:28:33 +0700

    Update gemini.js

--- api/gemini.js
+++ api/gemini.js
@@ -21,7 +21,7 @@ export default async function handler(req, res) {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
-                    "Authorization": "Bearer " + apiKey,
+                    "Authorization": "apiKey",
                     "Accept": "application/json"
                 },
                 body: JSON.stringify({
