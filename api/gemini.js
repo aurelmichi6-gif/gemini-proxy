e2294aeb3e058ef37368f0f8440f8d0e43aa5ba4
aurelmichi6-gif <aurelmichi6@gmail.com>
2026-04-15 09:50:30 +0700

    Update gemini.js

--- api/gemini.js
+++ api/gemini.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
 
     const systemPrompt = customPrompt
         ? customPrompt
-        : "Answer this question with short, direct, clear, on-topic answers:"
+        : ""
 
     try {
         const controller = new AbortController()
